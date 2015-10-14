e500a6acd6400b252ef39e3f5302cd884fbb0b9e
Isaac Byrne <isaacbyrne@Isaacs-Air.PK5001Z>
2015-10-14 13:36:01 -0600

    Fixed some styling

--- client/events/articles/article-list.js
+++ client/events/articles/article-list.js
@@ -2,6 +2,9 @@ Template.articleList.events({
 	'keyup .search-bar': function() {
 		Session.set("article-search-query", $('.search-bar').val());
 	},
+	'keyup .search-bar2': function() {
+		Session.set("article-search-query", $('.search-bar').val());
+	},
 	'click .load-more':function() {
 		articlesHandle.loadNextPage();
 	}
